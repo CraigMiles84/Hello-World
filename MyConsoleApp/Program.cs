b58150b433e3f8722952bb36c168e7da6b4583c8
Craig <craigmilo@hotmail.co.uk>
2026-01-05 19:50:22 +0000

    My latest message. Lesson 1

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello my name is Craig");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Craig";
+        string email = "Craig.milo@hotmail.co.uk";
+        int age = 41;
+        string carmake = "KIA";
+        string carmodel = "sportage";
+        {
+            Console.WriteLine($"My name is {name}, I am {age} years old. My email is {email}.\nI drive a {carmake} {carmodel}");
+        }
+    }
+}
