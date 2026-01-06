2013dd8e0568f39d8e5516d89cf2a990493636c0
Craig <craigmilo@hotmail.co.uk>
2026-01-06 18:12:47 +0000

    Lesson 1 commits, dictionarys and arrays

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,14 +1,34 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
+﻿
+Dictionary<string, int> people = new Dictionary<string, int>();
+
+    people.Add("Alice", 30);    
+    people.Add("Bob", 25);  
+    people.Add("Charlie", 35); 
+
+    foreach (var person in people)
     {
-        string name = "Craig";
-        string email = "Craig.milo@hotmail.co.uk";
-        int age = 41;
-        string carmake = "KIA";
-        string carmodel = "sportage";
-        {
-            Console.WriteLine($"My name is {name}, I am {age} years old. My email is {email}.\nI drive a {carmake} {carmodel}");
-        }
+        Console.WriteLine($"{person.Key} is {person.Value} years old.");
     }
+    
+
+int[]numbers = { 1, 2, 3, 4, 5 };
+
+    int sum = numbers.Sum();
+    Console.WriteLine($"The sum of the numbers is: {sum}"); 
+
+
+int[] numberss = { 100, 200, 300, 400, 500, 600, 700 };
+int largest = numberss[0];
+int smallest = numberss[1];
+
+foreach (int number in numbers)
+{
+    if (number > largest)
+        largest = number;
+
+    if (number < smallest)
+        smallest = number;  
 }
+
+Console.WriteLine($"The largest number in the array is {largest}");
+Console.WriteLine($"The smallest number in the array is {smallest}");
